24764af92a1524361cb747c36ee47be32f0aea90
ssplotnikov <devmode777@gmail.com>
2022-10-11 13:22:22 +0300

    nvim lsp installer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,106 +8,107 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- Setup nvim-cmp.
 local cmp = require("cmp")
 local source_mapping = {
-	buffer = "[Buffer]",
-	nvim_lsp = "[LSP]",
+    buffer = "[Buffer]",
+    nvim_lsp = "[LSP]",
     nvim_lua = "[Lua]",
-	cmp_tabnine = "[TN]",
-	path = "[Path]",
+    cmp_tabnine = "[TN]",
+    path = "[Path]",
 }
 local lspkind = require("lspkind")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			-- For `vsnip` user.
-			-- vim.fn["vsnip#anonymous"](args.body)
+    snippet = {
+        expand = function(args)
+            -- For `vsnip` user.
+            -- vim.fn["vsnip#anonymous"](args.body)
 
-			-- For `luasnip` user.
-			require("luasnip").lsp_expand(args.body)
+            -- For `luasnip` user.
+            require("luasnip").lsp_expand(args.body)
 
-			-- For `ultisnips` user.
-			-- vim.fn["UltiSnips#Anon"](args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
+            -- For `ultisnips` user.
+            -- vim.fn["UltiSnips#Anon"](args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-	}),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    }),
 
-	formatting = {
-		format = function(entry, vim_item)
-			vim_item.kind = lspkind.presets.default[vim_item.kind]
-			local menu = source_mapping[entry.source.name]
-			if entry.source.name == "cmp_tabnine" then
-				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-					menu = entry.completion_item.data.detail .. " " .. menu
-				end
-				vim_item.kind = ""
-			end
-			vim_item.menu = menu
-			return vim_item
-		end,
-	},
+    formatting = {
+        format = function(entry, vim_item)
+            vim_item.kind = lspkind.presets.default[vim_item.kind]
+            local menu = source_mapping[entry.source.name]
+            if entry.source.name == "cmp_tabnine" then
+                if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+                    menu = entry.completion_item.data.detail .. " " .. menu
+                end
+                vim_item.kind = ""
+            end
+            vim_item.menu = menu
+            return vim_item
+        end,
+    },
 
-	sources = {
-		-- tabnine completion? yayaya
+    sources = {
+        -- tabnine completion? yayaya
 
-		{ name = "cmp_tabnine" },
+        { name = "cmp_tabnine" },
 
-		{ name = "nvim_lsp" },
+        { name = "nvim_lsp" },
 
-		-- For vsnip user.
-		-- { name = 'vsnip' },
+        -- For vsnip user.
+        -- { name = 'vsnip' },
 
-		-- For luasnip user.
-		{ name = "luasnip" },
+        -- For luasnip user.
+        { name = "luasnip" },
 
-		-- For ultisnips user.
-		-- { name = 'ultisnips' },
+        -- For ultisnips user.
+        -- { name = 'ultisnips' },
 
-		{ name = "buffer" },
+        { name = "buffer" },
 
-	},
+    },
 })
 
 local tabnine = require("cmp_tabnine.config")
 tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = "..",
+    max_lines = 1000,
+    max_num_results = 20,
+    sort = true,
+    run_on_every_keystroke = true,
+    snippet_placeholder = "..",
 })
 
 local function config(_config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-		on_attach = function()
-			nnoremap("gd", function() vim.lsp.buf.definition() end)
-			nnoremap("K", function() vim.lsp.buf.hover() end)
-			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-			nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
-			nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
-                filter = function(code_action)
-                    if not code_action or not code_action.data then
-                        return false
-                    end
+    return vim.tbl_deep_extend("force", {
+        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        on_attach = function()
+            nnoremap("gd", function() vim.lsp.buf.definition() end)
+            nnoremap("K", function() vim.lsp.buf.hover() end)
+            nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+            nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
+            nnoremap("[d", function() vim.diagnostic.goto_next() end)
+            nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+            nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
+            nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
+                    filter = function(code_action)
+                        if not code_action or not code_action.data then
+                            return false
+                        end
 
-                    local data = code_action.data.id
-                    return string.sub(data, #data - 1, #data) == ":0"
-                end,
-                apply = true
-            }) end)
-			nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
-			nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
-			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-		end,
-	}, _config or {})
+                        local data = code_action.data.id
+                        return string.sub(data, #data - 1, #data) == ":0"
+                    end,
+                    apply = true
+                })
+            end)
+            nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
+            nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
+            inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+        end,
+    }, _config or {})
 end
 
 require("lspconfig").zls.setup(config())
@@ -125,21 +126,21 @@ require("lspconfig").solang.setup(config())
 require("lspconfig").cssls.setup(config())
 
 require("lspconfig").gopls.setup(config({
-	cmd = { "gopls", "serve" },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = true,
-		},
-	},
+    cmd = { "gopls", "serve" },
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+        },
+    },
 }))
 
 -- who even uses this?
 require("lspconfig").rust_analyzer.setup(config({
-	cmd = { "rustup", "run", "nightly", "rust-analyzer" },
-	--[[
+    cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+    --[[
     settings = {
         rust = {
             unstable_features = true,
@@ -151,62 +152,93 @@ require("lspconfig").rust_analyzer.setup(config({
 }))
 
 local opts = {
-	-- whether to highlight the currently hovered symbol
-	-- disable if your cpu usage is higher than you want it
-	-- or you just hate the highlight
-	-- default: true
-	highlight_hovered_item = true,
+    -- whether to highlight the currently hovered symbol
+    -- disable if your cpu usage is higher than you want it
+    -- or you just hate the highlight
+    -- default: true
+    highlight_hovered_item = true,
 
-	-- whether to show outline guides
-	-- default: true
-	show_guides = true,
+    -- whether to show outline guides
+    -- default: true
+    show_guides = true,
 }
 
 require("symbols-outline").setup(opts)
 
 local snippets_paths = function()
-	local plugins = { "friendly-snippets" }
-	local paths = {}
-	local path
-	local root_path = vim.env.HOME .. "/.vim/plugged/"
-	for _, plug in ipairs(plugins) do
-		path = root_path .. plug
-		if vim.fn.isdirectory(path) ~= 0 then
-			table.insert(paths, path)
-		end
-	end
-	return paths
+    local plugins = { "friendly-snippets" }
+    local paths = {}
+    local path
+    local root_path = vim.env.HOME .. "/.vim/plugged/"
+    for _, plug in ipairs(plugins) do
+        path = root_path .. plug
+        if vim.fn.isdirectory(path) ~= 0 then
+            table.insert(paths, path)
+        end
+    end
+    return paths
 end
 
 require("luasnip.loaders.from_vscode").lazy_load({
-	paths = snippets_paths(),
-	include = nil, -- Load all languages
-	exclude = {},
+    paths = snippets_paths(),
+    include = nil, -- Load all languages
+    exclude = {},
 })
 
+local on_attach = function()
+    nnoremap("gd", function() vim.lsp.buf.definition() end)
+    nnoremap("K", function() vim.lsp.buf.hover() end)
+    nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+    nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
+    nnoremap("[d", function() vim.diagnostic.goto_next() end)
+    nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+    nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
+    nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
+            filter = function(code_action)
+                if not code_action or not code_action.data then
+                    return false
+                end
+
+                local data = code_action.data.id
+                return string.sub(data, #data - 1, #data) == ":0"
+            end,
+            apply = true
+        })
+    end)
+    nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
+    nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
+    inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+end
+local lsp_flags = {
+    debounce_text_changes = 150,
+}
 require("nvim-lsp-installer").setup({
     automatic_installation = true,
     ui = {
         icons = {
-            server_installed = 'ok',
-            server_panding = 'pending',
-            server_uninstalled = 'uninstall'
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
         }
     }
 })
-require('lspconfig')['tsserver'].setup{
+require('lspconfig')['tsserver'].setup {
     on_attach = on_attach,
     flags = lsp_flags
 }
-require('lspconfig')['rust_analyzer'].setup{
+require('lspconfig')['rust_analyzer'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     -- Server-specific settings...
     settings = {
-      ["rust-analyzer"] = {}
+        ["rust-analyzer"] = {}
     }
 }
-require('lspconfig')['pyright'].setup{
+require('lspconfig')['pyright'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['sumneko_lua'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
 }
